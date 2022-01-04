46fed20c81e06dcfed6ac657756bac720ad27bbc
Maxwell Lasky <mlasky46@gmail.com>
2022-01-04 10:35:42 -0700

    Add crypto bot

--- handler.js
+++ handler.js
@@ -39,3 +39,34 @@ module.exports.clap = async (event) => {
 
   return { statusCode: 200 };
 };
+
+module.exports.crypto_fetch = async (event) => {
+  const { text, user_id, response_url } = querystring.parse(event.body);
+  if (!text) {
+    return {
+      statusCode: 200,
+      body: `You need to submit text in order to use crypto_fetch`,
+      headers: { "X-Slack-No-Retry": 1 },
+    };
+  }
+
+  const joinedTokens = text.split(" ").join(",");
+
+  const results = await axios.get(
+    `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${joinedTokens}&tsyms=${USD}`
+  );
+
+  const response = JSON.stringify({
+    attachments: [
+      {
+        text: JSON.stringify(results.data, null, 4),
+      },
+    ],
+    response_type: "in_channel",
+    text: `<@crypto_fetch}>`,
+  });
+
+  await axios.post(response_url, response).catch((e) => console.log(e));
+
+  return { statusCode: 200 };
+};
